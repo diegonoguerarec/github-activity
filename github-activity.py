feb18c2396d572e00768f378edc58c435a281688
diegao <diegonoguerarec@gmail.com>
2025-04-13 18:20:49 -0300

    Funciona para PushEvent

--- github-activity.py
+++ github-activity.py
@@ -14,12 +14,27 @@ def main():
     # Peticion
     r = requests.get(f"https://api.github.com/users/{nombre}/events")
 
+    print()
     if r.status_code == 200:
-        actividad = r.json()
-        print(actividad)
+        actividades = r.json()
+        
+        for actividad in actividades: ## Ciclo para pasar por todos los eventos
+            if actividad["type"] == "PushEvent": ## Si es un evento de push
+                cantidad_commits = len(actividad["payload"]["commits"])
+                nombre_repo = actividad["repo"]["url"]
+
+                # Detalles de los commits
+                print("-"*90)
+                print(f"Push de {cantidad_commits} commits a -> {nombre_repo}")
+                print(f"\t {"Autor"[:10]:<11} {"Mensaje"[:50]:<51}")
+                print("\t"+"-"*82)
+                for commit in actividad["payload"]["commits"]:
+                    print(f"\t {commit["author"]["name"][:10]:<11} {commit["message"][:50]:<51}")
+
+            print()
 
         with open("actividad.json", mode="w") as f:
-            json.dump(actividad, f, indent=4)
+            json.dump(actividades, f, indent=4)
     
     else:
         print("Error al obtener datos...")
